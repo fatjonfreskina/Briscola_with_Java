4ae85d24d7c4870afaa71fd09ea8d1bb9e25a5a0
fatjonfreskina <fatjonfreskina@gmail.com>
2022-02-05 23:39:45 +0100

    a

--- src/Player.java
+++ src/Player.java
@@ -17,23 +17,40 @@ public class Player{
         this.wonCards.add(card2);
     }
 
-    public void pickCard(String tmpCard){
-        playerCards.add(tmpCard);
-        //System.out.println("Card added to players deck");
+    public void pickCard(Mazzo mazzo){
+        String card = mazzo.getCard();
+        playerCards.add(card);
     }
 
     public void dispPlayerCard(){
         System.out.println(this.playerCards);
     }
 
-    public String throwCard(){
+    public String randomThrowCard(){        //this is used by the dumb pc
         int size = playerCards.size();
         int randomNum = ThreadLocalRandom.current().nextInt(1, size);
         String tmpCard = playerCards.get(randomNum);
-        this.playerCards.remove(randomNum);
+        this.playerCards.remove(tmpCard);
         return tmpCard;        
     }
-    
+
+    public String throwCard(){
+        Scanner sc= new Scanner(System.in);
+        while(sc.hasNext()){
+            System.out.printf("Choose a card to throw: " + this.playerCards + "\n");
+            System.out.println("Enter the card you want to throw: ");
+            String card = sc.next();
+            System.out.printf("You choose: " + card + "\n");
+            if (this.playerCards.contains(card)){
+                this.playerCards.remove(card);
+                
+                break;
+            }
+        sc.close();
+        return card;
+    }
+}
+
 }
 
             
