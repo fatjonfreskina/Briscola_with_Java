4e51defa7f3ef4d701deeb4c8884de9bab880621
fatjonfreskina <fatjonfreskina@gmail.com>
2022-02-05 23:39:30 +0100

    throw card method to be fixed

--- src/App.java
+++ src/App.java
@@ -9,41 +9,33 @@ import java.util.concurrent.ThreadLocalRandom;
 public class App {
     public static void main(String[] args) throws Exception {
 
-        
-
-        GameLoop game = new GameLoop(); // the method prints Game Began
-        Mazzo mazzo = new Mazzo();
+        Mazzo mazzo = new Mazzo();     //mazzo object has getCard, hasCards
         Player player1 = new Player(); // prints Player created
         Player player2 = new Player(); // prints Player created
 
-        for (int i = 0; i < 3; i++) {
-            player1.pickCard(mazzo);// 
+        for (int i = 0; i < 3; i++) { // First 3 cards
+            player1.pickCard(mazzo);
+            player2.pickCard(mazzo);
+        }
 
-
-        String tmpCard1 = player1.throwCard();
-        System.out.printf("player 1 throws: " + tmpCard1 + '\n');
-        String tmpCard2 = player2.throwCard();
-        System.out.printf("player 2 throws: " + tmpCard2 + '\n');
-        System.out.println("player 1 won for some reason");
-        
-        player1.addToWonCards(tmpCard1, tmpCard2);
-        System.out.printf("Player 1 won cards: " + player1.wonCards + '\n');
-
-        System.out.printf("Calling checkGame: " + game.checkGame(mazzo) + '\n');
-        
-        player1.dispPlayerCard();
-        player2.dispPlayerCard();
-
-        System.out.println(mazzo.hasCards());
-        
+        player1.throwCard(); // test this method
 
     }
 
-final Player isWinner(Player player1, String card1, Player player2, String card2){
+final void isWinner(Player player1, String card1, Player player2, String card2){
     int randomNum = ThreadLocalRandom.current().nextInt(1, 2);
-    if (randomNum == 1){
-        return player1;}
+    if (randomNum == 1){ //CASE PLAYER 1 WON -> ADD TO PLAYER 1 WONCARDS
+        player1.addToWonCards(card1, card2);}
     else {
-        return player2;}
-    }
+        player2.addToWonCards(card1, card2);}
+}
+
+
+final void gameTurn(Player player1, Player player2, Mazzo mazzo){
+    String card1 = player1.throwCard();
+    String card2 = player2.randomThrowCard();
+    isWinner(player1, card1, player2, card2);
+    player1.pickCard(mazzo);
+    player2.pickCard(mazzo);
+}
 }
